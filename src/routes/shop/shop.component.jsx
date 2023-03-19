eb9b06e858bdc7999b82c83ed37b0acda77b796c
nipunaupeksha <nipuna.upeksha@gmail.com>
2023-03-19 20:41:27 +0530

    categories implementation

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,21 @@
 import React, { useContext } from 'react'
-import { ProductsContext } from '../../contexts/products.context'
-import ProductCard from '../../components/product-card/product-card.component'
-import './shop.styles.scss'
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import './shop.styles.scss';
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext)
-  return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCard key={products.id} product={product} />
-      ))}
-    </div>
-  )
-}
+  const { categoriesMap } = useContext(CategoriesContext);
 
-export default Shop
+  return (
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
+    </div>
+  );
+};
+
+export default Shop;
