d341ab7f1f29a67c8fc1b98805500df22f0fb13a
nipunaupeksha <nipuna.upeksha@gmail.com>
2023-03-19 20:56:52 +0530

    caegories section implemented

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,18 @@
-import React, { useContext } from 'react'
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import CategoriesPreview from '../categories-preview/categories-preview.component'
+import Category from '../category/category.component'
 
-import './shop.styles.scss';
+import './shop.styles.scss'
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className='shop-container'>
-      {Object.keys(categoriesMap).map((key) => {
-        const products = categoriesMap[key];
-        return <CategoryPreview key={key} title={key} products={products} />;
-      })}
-    </div>
-  );
-};
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
+  )
+}
 
-export default Shop;
+export default Shop
